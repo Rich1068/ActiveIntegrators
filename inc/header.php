706e9995d13faf5f3054eef1b9e59d15556ec550
kovlor13 <127574325+kovlor13@users.noreply.github.com>
2024-05-25 23:18:49 +0800

    Update header.php
    Design - Header Container

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,17 @@
             font-family: 'Times New Roman', Times, serif;
         }
 
+        .cart-container {
+            display: flex;
+            align-items: center;
+            border-radius: 5px;
+            padding: 10px 60px; /* Adjust padding to make the container wider */
+            color: white;
+            text-align: center;
+            margin-right: 50px; /* Add margin to the right to separate from the right edge */
+            box-shadow: 0 0 0 2px rgba(255, 255, 255, 1); /* Make the outline skinnier and pure white */
+        }
+
         .cart-icon {
             position: relative;
             display: inline-block;
@@ -25,30 +36,40 @@
             font-weight: normal; /* Ensure normal font weight */
         }
 
-        /* Adjust the position of "View My Cart" text */
         .view-cart-text {
-            margin-right: 10px; /* Add some space between the text and the cart icon */
-            color: white; /* Ensure text color is white */
+            margin-right: 10px;
+            color: white;
         }
 
-        /* Remove any border or outline from the cart icon */
         .cart-icon i {
-            border: none; /* Remove border */
-            outline: none; /* Remove outline */
+            border: none;
+            outline: none;
         }
-    
+
+        .container-left {
+            flex: 1; /* Take up remaining space */
+            overflow: hidden; /* Hide overflow */
+            text-overflow: ellipsis; /* Add ellipsis for overflow text */
+            white-space: nowrap; /* Prevent text wrapping */
+        }
+
+        .container-right {
+            margin-left: auto; /* Push the right container to the right edge */
+            flex: 1; /* Take up remaining space */
+            text-align: right; /* Align content to the right */
+        }
     </style>
                 
-    <nav class="navbar navbar-expand-lg navbar-dark   ">
+    <nav class="navbar navbar-expand-lg navbar-dark">
         <a href="index.php" class="navbar-brand">
-            <h3 class="px-5">
-                <img src="upload/Logo.png" width="100" height="100" style="background: transparent;"></img> Scholar's Secret
+            <h3 class="px-5 container-left">
+                <img src="upload/Logo.png" width="100" height="100" style="background: transparent;"> Scholar's Secret
             </h3>   
         </a>
         <button class="navbar-toggler"
             type="button"
                 data-toggle="collapse"
-                data-target = "#navbarNavAltMarkup"
+                data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup"
                 aria-expanded="false"
                 aria-label="Toggle navigation"
@@ -60,7 +81,7 @@
             <div class="mr-auto"></div>
             <div class="navbar-nav">
                 <a href="cart.php" class="nav-item nav-link active">
-                    <h5 class="px-5 cart">
+                    <div class="cart-container container-right" style="border-color: white;">
                         <span class="view-cart-text">View My Cart</span>
                         <div class="cart-icon">
                             <i class="fas fa-shopping-cart"></i>
@@ -78,10 +99,9 @@
                                 ?>
                             </span>
                         </div>
-                    </h5>
+                    </div>
                 </a>
             </div>
         </div>
-
     </nav>
 </header>
