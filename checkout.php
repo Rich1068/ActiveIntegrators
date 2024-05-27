c10617b7c80ece243b88192eef0f5dc9b191618c
Richard Darwin Sy <sy.richarddarwin@auf.edu.ph>
2024-05-27 22:13:54 +0800

    updated checkout

--- checkout.php
+++ checkout.php
@@ -15,12 +15,15 @@ $result = $db->getData($pids);
 
 $line_items = [];
 $total = 0;
+$description = "";
+date_default_timezone_set('Asia/Manila');
+$currentDateTime = (new DateTime())->format('F j, Y, g:i:s A');
 
 // Loop through the fetched product data
 while ($row = $result->fetch_assoc()) {
     $quantity = intval($_SESSION['cart'][$row['id']]);
     $unit_amount = floatval($row['current_price']) * 100; // Convert to cents
-
+    $unit_total = floatval($row['current_price']) * $quantity;
     // Add item to the Stripe line items array
     $line_items[] = [
         "quantity" => $quantity,
@@ -32,16 +35,22 @@ while ($row = $result->fetch_assoc()) {
             ]
         ]
     ];
-
+    
     // Calculate the total
     $total += $unit_amount * $quantity;
-}
 
+    $description .= "<h1>{$row['name']}</h1><h2>Quantity: {$quantity}</h2><h3>Price: ₱ {$unit_total}</h3>";
+}
+$datetime .= "<h4> {$currentDateTime}<br/>";
 
 $checkout_session = \Stripe\Checkout\Session::create([
     "mode" => "payment",
     "success_url" => "http://localhost/stripe/ActiveIntegrators/success.php",
+    "cancel_url" => "http://localhost/stripe/ActiveIntegrators/cart.php",
     "line_items" => $line_items,
+    "payment_intent_data" => [
+        "description" => $description,
+    ]
 ]);
 
 
